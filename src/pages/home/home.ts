429c4261ed04a5f2ee52766b8e7b23880248b99b
azhar <mazhar@ptnicec.ac.th>
2018-11-08 18:04:37 +0700

    sent component

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ErrorHandler } from '@angular/core';
+import { AlertController, NavController,ActionSheetController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,45 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public alertCtrl:AlertController,
+    public actionSheet:ActionSheetController,
+    public navCtrl: NavController) {
 
   }
 
+  showAction(){
+    const acSheet = this.actionSheet.create({
+      title:'Test Action',
+      buttons:[
+        {
+        text: 'Information',
+        role: 'Info',
+        handler: () => {
+          console.log('Destructive clicked');
+      }
+     }
+    ]
+  });
+  acSheet.present();
+ }
+
+
+ showAlert1(){
+   const alert = this.alertCtrl.create({
+     title: 'Confirm Answer!!',
+     subTitle: 'Azhar Musor',
+     buttons: [
+     { 
+      text: 'OK'
+    },
+  {
+      text: 'Cancel',
+      handler: () => {
+        console.log("Cancel");
+      },
+    }] 
+   });
+   alert.present();
+ }
 }
+
